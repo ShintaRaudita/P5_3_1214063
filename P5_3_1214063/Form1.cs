11ad1eea4ee26b2a7cab0284a9e6abfdbc433de4
ShintaRaudita <98589423+ShintaRaudita@users.noreply.github.com>
2023-01-25 03:54:53 +0700

    Add files via upload

---
+++ P5_3_1214063/Form1.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace P5_3_1214063
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void tsbMouse_Click(object sender, EventArgs e)
+        {
+            if (rbMouse.Checked == true)
+            {
+                tslKetGambar.Text = "Cocok! Ini Adalah Gambar Mouse";
+            }
+            else
+            {
+                tslKetGambar.Text = "Tidak Cocok! Anda memilih gambar yang salah \n ini adalah gambar mouse";
+            }
+        }
+
+        private void tsbMonitor_Click(object sender, EventArgs e)
+        {
+            if (rbMonitor.Checked == true)
+            {
+                tslKetGambar.Text = "Cocok! Ini Adalah Gambar Monitor";
+            }
+            else
+            {
+                tslKetGambar.Text = "Tidak Cocok! Anda memilih gambar yang salah \n ini adalah gambar monitor";
+            }
+        }
+
+        private void tsbPrinter_Click(object sender, EventArgs e)
+        {
+            if (rbPrinter.Checked == true)
+            {
+                tslKetGambar.Text = "Cocok! Ini Adalah Gambar Printer";
+            }
+            else
+            {
+                tslKetGambar.Text = "Tidak Cocok! Anda memilih gambar yang salah \n ini adalah gambar Printer";
+            }
+        }
+
+        private void tsbKeyboard_Click(object sender, EventArgs e)
+        {
+            if (rbKeyboard.Checked == true)
+            {
+                tslKetGambar.Text = "Cocok! Ini Adalah Gambar Keyboard";
+            }
+            else
+            {
+                tslKetGambar.Text = "Tidak Cocok! Anda memilih gambar yang salah \n ini adalah gambar Keyboard";
+            }
+        }
+
+        private void tsbScanner_Click(object sender, EventArgs e)
+        {
+            if (rbScanner.Checked == true)
+            {
+                tslKetGambar.Text = "Cocok! Ini Adalah Gambar Scanner";
+            }
+            else
+            {
+                tslKetGambar.Text = "Tidak Cocok! Anda memilih gambar yang salah \n ini adalah gambar Scanner";
+            }
+        }
+    }
+}
